682470fff5e9edad9a346510f03cb13618d381c7
gerardoaquino25 <gerardo.aquino25@gmail.com>
2015-08-27 12:33:26 -0300

    .

---
+++ ReportCreator/Entities/Tipo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportCreator.Entities
+{
+    public class Tipo
+    {
+        public int id;
+        public string descripcion { get; set; }
+
+        public Tipo(int id, string descripcion)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+        }
+
+        public override bool Equals(Object tipo)
+        {
+            if (tipo == null)
+                return false;
+
+            Tipo p = tipo as Tipo;
+            if ((System.Object)p == null)
+                return false;
+
+            return id == p.id;
+        }
+    }
+}
